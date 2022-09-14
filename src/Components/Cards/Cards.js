38ac747b7f9e81a835c4f7cbef5d159e91095b3a
SyedAliImran <syed.ali.imran456@gmail.com>
2022-09-14 14:01:15 +0500

    commit 3

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -10,15 +10,15 @@ import {programs} from '../Cards/objects'
 const Cards = () => {
   return (
     <>
-    <div className="container">
+    <div className="container mns">
       <div className="cards_parent">
     <div className="carddd">
-      <h1>Programs</h1>
+      <h2>Programs</h2>
       <div className="row aptDe">
-      {/* style={{width: '18rem'}} */}
+      
       { programs.map((program,key)=>{
           return(
-        <div className="col-md-3 aptDe " key={key}>
+        <div className="col-lg-3 col-md-4 aptDe " key={key} >
         <div className="card" >
         <img className="card-img-top" src={program.image} alt="gym" />
         <div className="card-body">
--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -9,19 +9,18 @@ const AboutPage = () => {
         <div className="gymInfo">
        
           <div className="row">
-            <div className="col-md-5">
-              <img
+            <div className=" col-md-5 col-sm-12 col-lg-5 imggg img-fluid">
+              {/* <img
                 src="https://talkgolfscotland.co.uk/wp-content/uploads/2020/09/gym-4k.jpeg"
                 className="img-fluid"
                 alt="gym"
-              />
+              /> */}
             </div>
-            <div className="col-md-7 about_Text">
-            <h1>About Life Fitness</h1>
+            <div className="col-md-7 col-sm-12 col-lg-7  about_Text">
+            <h2>About Life Fitness</h2>
                 <p>At Life Fitness, we are committed to helping you reach your fitness goals. Our experience runs deep and our trainers know just how to get your adrenaline rushing. We believe 
                 in following the latest fitness trends, and we also invest in time-tested techniques.</p>
-              
-                <p>Our gym features the best state-of-the-art strength and cardio equipment, and personal training classes that get you on track when you’re not. We believe that when you come to VelocityX you’re not just joining another gym, you’re stepping into a world of like-minded people
+               <p>Our gym features the best state-of-the-art strength and cardio equipment, and personal training classes that get you on track when you’re not. We believe that when you come to Life Fitness you’re not just joining another gym, you’re stepping into a world of like-minded people
                  who are here to give you everything you need to get a solid workout.</p>
             
             </div>
